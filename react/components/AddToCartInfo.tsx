c7d0320d19e143d0f96eb02aba45631e1398e52c
JeferProgramer <jefer.talento.11@gmail.com>
2022-09-17 13:38:49 -0500

    feat finish component add cart to info

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -1,13 +1,49 @@
-import React from "react"
-import {useProduct} from"vtex.product-context"
+/* eslint-disable padding-line-between-statements */
+import React from 'react'
+import { useProduct } from 'vtex.product-context'
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
+import { generateBlockClass } from '@vtex/css-handles'
 
-const AddToCartInfo = () => {
+import ButtonGroup from './ButtonGroup'
+import styles from './styles.css'
+
+const AddtoCartInfo = ({ blockClass }: { blockClass: string }) => {
+  const container = generateBlockClass(styles.container, blockClass)
+  const containerTotal = generateBlockClass(styles.containerTotal, blockClass)
+  const card = generateBlockClass(styles.card, blockClass)
   const productInfo = useProduct()
-  console.log("Este producto tiene esta info: ",productInfo)
-  return(
-    <div>Aqui renderizamos nuestra informacion</div>
+  const {
+    orderForm: { items, totalizers },
+  } = useOrderForm()
+
+  console.log('estos son mis items ', items)
+  console.log('estos son mis totales ', totalizers)
+  console.log('este es mi producto ', productInfo)
+
+  return (
+    <div className={container}>
+      {items.map((item: any, index: number) => {
+        console.log(item)
+        return (
+          <div key={index} className={card}>
+            <div>
+              <img src={item.imageUrls.at1x} alt="product" />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>Id : {item.id}</p>
+              <p>${item.price / 100}</p>
+              <p>Cantidad: {item.quantity}</p>
+            </div>
+          </div>
+        )
+      })}
+      <div className={containerTotal}>
+        <p>Total: {totalizers[0]?.value / 100}</p>
+        <ButtonGroup />
+      </div>
+    </div>
   )
 }
 
-
-export default AddToCartInfo
+export default AddtoCartInfo
